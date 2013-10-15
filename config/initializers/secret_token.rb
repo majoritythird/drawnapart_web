0a42294ec1870f2246c1aefcf685189c51e010b7
Wes Gibbs <wesgibbs@gmail.com>
2013-10-15 12:29:44 -0700

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Budgee::Application.config.secret_key_base = '3836956a213dda5c0fa72e43ba1b9be3f6536a49994bc59b64261affeaaa27758c3e0a80b058d2015f6c4b84a7eca811fa3c5e0b3799147ef97c0dd323e69f6d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Budgee::Application.config.session_store :cookie_store, key: '_budgee_session'
