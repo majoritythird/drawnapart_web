057381942aaa65d1c7dd2647517c043d7d014c52
Wes Gibbs <wesgibbs@gmail.com>
2013-10-18 20:00:11 -0700

    Cleanup

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -2,14 +2,6 @@ class Api::V1::RegistrationsController < Users::RegistrationsController
 
   respond_to :json
 
-  # before_filter :configure_permitted_parameters
-  # skip_before_filter :authenticate_user!
-  # skip_before_filter :verify_authenticity_token
-
-  # private
-
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.for(:sign_up) << {person_attributes: [:name]}
-  # end
+  skip_before_filter :verify_authenticity_token
 
 end
